7689fcd3552b452a91bc26005b2879700b93baf8
Simon Sobisch <simonsobisch@web.de>
2023-06-12 21:28:01 +0200

    output error messages when downloading vscode server (#39)
    * output download messages / progress
    
    helping the user to recognize/handle issues, see discussion in #24
    
    * :lipstick:
    
    ---------
    
    Co-authored-by: Jean Pierre <jeanp413@hotmail.com>

--- src/serverSetup.ts
+++ src/serverSetup.ts
@@ -317,9 +317,9 @@ if [[ ! -f $SERVER_SCRIPT ]]; then
     pushd $SERVER_DIR > /dev/null
 
     if [[ ! -z $(which wget) ]]; then
-        wget --tries=3 --timeout=10 --continue --quiet -O vscode-server.tar.gz $SERVER_DOWNLOAD_URL
+        wget --tries=3 --timeout=10 --continue --no-verbose -O vscode-server.tar.gz $SERVER_DOWNLOAD_URL
     elif [[ ! -z $(which curl) ]]; then
-        curl --retry 3 --connect-timeout 10 --location --silent --output vscode-server.tar.gz $SERVER_DOWNLOAD_URL
+        curl --retry 3 --connect-timeout 10 --location --show-error --silent --output vscode-server.tar.gz $SERVER_DOWNLOAD_URL
     else
         echo "Error no tool to download server binary"
         print_install_results_and_exit 1
