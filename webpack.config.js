217ce1ab1eb2a38996d306935432829f68b0065d
Baptiste Augrain <daiyam@zokugun.org>
2026-04-27 16:00:34 +0200

    build: use npm instead of yarn

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const config = {
 	target: 'node',
 	entry: './src/extension.ts',
 	output: {
-		path: path.resolve(__dirname, 'out'),
+		path: path.resolve(__dirname, 'lib'),
 		filename: 'extension.js',
 		libraryTarget: "commonjs2",
 		devtoolModuleFilenameTemplate: "../[resource-path]",
